80e736f9e113b4b59bae94e08d47d1adf9462c99
jordansbenjamin <14327@coderacademy.edu.au>
2023-06-01 09:55:46 +1000

    Added method for next question in quizbrain class

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from quiz_brain import QuizBrain
 from question_model import Question
 from data import question_data
 
@@ -5,3 +6,6 @@ question_bank = []
 for question in question_data:
     new_question = Question(question['text'], question['answer'])
     question_bank.append(new_question)
+
+test = QuizBrain(question_bank)
+test.next_question()
--- quiz_brain.py
+++ quiz_brain.py
@@ -1,5 +1,13 @@
+
 class QuizBrain:
     
     def __init__(self, q_list):
         self.question_number = 0
         self.questions_list = q_list
+
+    def next_question(self):
+        index = self.question_number
+        question = self.questions_list[index]
+        index += 1
+        answer = input(f"Q.{index}: {question.text} (True/False): ")
+

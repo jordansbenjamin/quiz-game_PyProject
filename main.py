3db0b86a5a5a8904d67ddcc045a1d5c047aaeb0b
jordansbenjamin <14327@coderacademy.edu.au>
2023-06-01 10:05:12 +1000

    Added still has questions method and fixed a bug

--- main.py
+++ main.py
@@ -7,5 +7,8 @@ for question in question_data:
     new_question = Question(question['text'], question['answer'])
     question_bank.append(new_question)
 
-test = QuizBrain(question_bank)
-test.next_question()
+quiz = QuizBrain(question_bank)
+
+#if quiz still has questions remaining
+while quiz.still_has_questions():
+    quiz.next_question()
--- quiz_brain.py
+++ quiz_brain.py
@@ -5,9 +5,12 @@ class QuizBrain:
         self.question_number = 0
         self.questions_list = q_list
 
+    def still_has_questions(self):
+        length = len(self.questions_list)
+        return self.question_number < length
+
     def next_question(self):
-        index = self.question_number
-        question = self.questions_list[index]
-        index += 1
-        answer = input(f"Q.{index}: {question.text} (True/False): ")
+        question = self.questions_list[self.question_number]
+        self.question_number += 1
+        input(f"Q.{self.question_number}: {question.text} (True/False): ")
 

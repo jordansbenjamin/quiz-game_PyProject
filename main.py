0a8016e6288cd674a3da43b11ccbd2e737d2a6f1
jordansbenjamin <14327@coderacademy.edu.au>
2023-06-01 10:39:21 +1000

    Added new data and made it work as expected

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from data import question_data
 
 question_bank = []
 for question in question_data:
-    new_question = Question(question['text'], question['answer'])
+    new_question = Question(question['question'], question['correct_answer'])
     question_bank.append(new_question)
 
 quiz = QuizBrain(question_bank)
